77f6b1865089bc731705ba55ae5319552da6ef32
HeskeyBaozi <hezhiyu233@foxmail.com>
2020-03-05 03:25:53 +0800

    core: adjust babel config

--- packages/core/babel.config.cjs
+++ packages/core/babel.config.cjs
@@ -5,11 +5,12 @@ module.exports = {
       '@babel/env',
       {
         targets: {
-          chrome: '49',
+          browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 11'],
           node: '10'
         },
         modules: process.env.BUILD_COMMONJS_MODULE ? 'commonjs' : false
       }
     ]
-  ]
+  ],
+  plugins: ['@babel/plugin-proposal-class-properties']
 };
