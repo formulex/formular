96b73003166eaee27c905fbb7ad638f7a5b1df92
HeskeyBaozi <hezhiyu233@foxmail.com>
2020-04-21 01:59:43 +0800

    remove middleware

--- packages/core/src/src2/models/form.ts
+++ packages/core/src/src2/models/form.ts
@@ -1,15 +1,7 @@
-import {
-  types,
-  Instance,
-  getType,
-  addMiddleware,
-  IDisposer,
-  getPath
-} from 'mobx-state-tree';
+import { types, Instance, getType } from 'mobx-state-tree';
 import { Field, FieldConfig, FieldInstance, createField } from './field';
 import { IReactionDisposer, autorun } from 'mobx';
 import { setIn } from '../utils';
-import { getDispatch } from '../middleware';
 import { Setup, ResolverContextManager, getResolvers } from '../sideEffect';
 
 export interface FieldRegisterConfig extends Omit<FieldConfig, 'name'> {}
@@ -107,30 +99,7 @@ export const Form = types
     reset(initialValues: any = self.initialValues) {
       self.initialize(initialValues || {});
     }
-  }))
-  .actions((self) => {
-    let disposer: IDisposer | undefined;
-    return {
-      afterCreate() {
-        disposer = addMiddleware(self, (call, next) => {
-          if (
-            call.name === 'setValue' &&
-            typeof call.context.validate === 'function'
-          ) {
-            getDispatch(
-              `${call.name}${getPath(call.context)}`,
-              100,
-              'validate'
-            )(call);
-          }
-          next(call);
-        });
-      },
-      beforeDestroy() {
-        disposer?.();
-      }
-    };
-  });
+  }));
 
 export type FormInstance = Instance<typeof Form>;
 
