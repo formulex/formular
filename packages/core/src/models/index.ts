8e7bb1889684e0816b8edb80935db518c9f1ffba
HeskeyBaozi <hezhiyu233@foxmail.com>
2020-07-17 15:13:23 +0800

    core: add submit

--- packages/core/src/models/index.ts
+++ packages/core/src/models/index.ts
@@ -1,4 +1,10 @@
 export { isFieldInstance, createField } from './field';
 export type { FieldInstance, FieldConfig, FieldRegisterConfig } from './field';
 export { isFormInstance, createForm } from './form';
-export type { FormInstance, FormConfig, FormValidateCallOptions } from './form';
+export type {
+  FormInstance,
+  FormConfig,
+  FormValidateCallOptions,
+  OnFinishFailed,
+  OnFinish
+} from './form';
